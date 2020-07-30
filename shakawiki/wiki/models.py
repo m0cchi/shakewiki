9fb62b138e4c5997e4930802066ef8b533270dfb
mocchi <boom.boom.planet@gmail.com>
2020-07-30 21:48:35 +0900

    add Article model

--- shakawiki/wiki/models.py
+++ shakawiki/wiki/models.py
@@ -1,5 +1,17 @@
+import markdown
 from django.db import models
+from mdx_gfm import GithubFlavoredMarkdownExtension
 
 
 class Article(models.Model):
-    path = models.CharField(max_length=512)
+  path = models.CharField(max_length=512)
+  body = models.TextField(blank=True)
+  markdown_body = models.TextField(blank=True)
+
+  def save(self):
+    self.body = markdown.markdown(
+        self.markdown_body, extensions=[GithubFlavoredMarkdownExtension()])
+    return super().save()
+
+  def __str__(self):
+    return '{} - {}'.format(self.path[0:30], self.body[0:30])
